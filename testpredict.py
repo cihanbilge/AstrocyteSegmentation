d8e377cd0b22c8ac9332934ce0809f741a4e19bb
cihanbilge <kayasa89@math.uh.edu>
2020-03-08 14:42:04 +0300

    Update testpredict.py

--- testpredict.py
+++ testpredict.py
@@ -1,13 +1,8 @@
-#from visual3_formodal4_reducelr import *
-from visual3_formodal333 import *
 
-#import numpy as np
-#from ternausLike import *
+from GESU_net import *
 import os
-#from lates_up import *
 import keras.backend as K
 import tensorflow as tf
-#tf.enable_eager_execution()
 from keras.models import Model
 
 
@@ -15,16 +10,15 @@ from data import *
 import time
 mydata = dataProcess(128,128)
 t = time.time()
-path='/home/bkayasandik/PycharmProjects/untitled11/data/train/tests_results_brainstem_formodel2'
+path='../data/tests_results'
 
 names = mydata.create_test_data()
 imgs_test = mydata.load_test_data()
 myunet = myUnet()
 
 model = myunet.get_unet()
-print(model.summary())
 
-model.load_weights('unet_forModel_real4again2lr_againlarger2.hdf5')
+model.load_weights('GESU_net_Modal.hdf5')
 
 imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
 
@@ -37,7 +31,7 @@ for i in range(1,imgs_mask_test.shape[0]):
     #    img = np.zeros((128,128,1), dtype=np.uint8)
     img = array_to_img(img)
 
-    img.save("/home/bkayasandik/PycharmProjects/untitled11/data/train/tests_results_brainstem_formodel2/%d.tif"%names[i])
+    img.save("../data/tests_results_processed/%d.tif"%names[i])
 
 myAugdata = myAugmentation()
 myAugdata.connComp()
